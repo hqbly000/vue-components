4bbc4e5d8080ffa1109f3fdc07d056da052bb764
hqbly <yygoodcccchu@gmail.com>
2022-09-01 15:11:00 +0800

    update

---
+++ src/components/list/main.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <p :key="item" v-for="item in list">{{ item }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      list: ['a', 'b']
+    }
+  }
+}
+</script>
+
+<style></style>
